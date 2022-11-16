d22f8be2e96b6a64a1f54dfb298a889b19088845
Hchopra10kview <102751770+Hchopra10kview@users.noreply.github.com>
2022-11-16 22:11:54 +0530

    server

--- app.js
+++ app.js
@@ -6,11 +6,21 @@ var bodyParser  = require('body-parser');
 var errorhandler = require('errorhandler');
 var http        = require('http');
 var path        = require('path');
+var https = require('https');
 var request     = require('request');
 var routes      = require('./routes');
 var activity    = require('./routes/activity');
+const fs = require('fs');
 
 var app = express();
+
+var key = fs.readFileSync('../selfsigned.key');
+var cert = fs.readFileSync('../selfsigned.crt');
+
+let options = {
+  key:key,
+  cert: cert
+}
 
 // Configure Express
 app.set('port', process.env.PORT || 3000);
@@ -38,6 +48,6 @@ app.post('/journeybuilder/validate/', activity.validate );
 app.post('/journeybuilder/publish/', activity.publish );
 app.post('/journeybuilder/execute/', activity.execute );
 
-http.createServer(app).listen(app.get('port'), function(){
+https.createServer(options, app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
